588bf89aa8f44088727fc9e94cbd3e766cb97fcd
Peter DeVita <mewtwo2643@yahoo.com>
2020-08-14 19:54:32 -0400

    Add TOC and Content components, update Readme

---
+++ www/src/js/components/TOC.js
@@ -0,0 +1,12 @@
+import { Component } from "preact";
+import styles from "./TOC.scss"
+
+class TOC extends Component {
+    render(props, state, context) {
+        return <div class={styles.TOC}>
+
+        </div>;
+    }
+}
+
+export default TOC
