0248f252a374a279ea33c1cea71e40f768c9287c
phr5493 <35848711+phr5493@users.noreply.github.com>
2020-09-17 11:08:01 -0400

    Change the main dict to a global variable so it could be used better, created a table of contents dict for future update, fixed overwriting problem with the footnotes files, added full text to JSON text item, and added hyperlinks to other chapters and for the index.

--- tools/epub_reader.py
+++ tools/epub_reader.py
@@ -6,6 +6,21 @@ from bs4 import BeautifulSoup
 #Convert html to markdown
 #Prints mardown to .json files
 
+page_number = 0
+
+main_dict = {
+    "number": 0,
+    "text": "",
+    "before": "",
+    "after": "",
+    "cross-references": [],
+    "footnotes": []
+}
+
+toc_dict = {
+
+}
+
 def main():
     table_of_contents = []
     list_of_contents = list()
@@ -32,7 +47,7 @@ def main():
         temp = soup.find(id=x)
         list_of_contents.append(temp.get('href'))
 
-    curr_dict = create_main_dict(1)
+    create_main_dict(1)
 
     #Iterates through the page file names and converts only the relevant pages into markdown
     for x in list_of_contents:
@@ -43,9 +58,16 @@ def main():
             main_text = main_text.replace("OEBPS", 'OutDocs')
             main_text = main_text.replace(".htm", '.json')
 
+            footnote_text = x
+            footnote_text = footnote_text.replace("OEBPS", 'Footnotes')
+            footnote_text = footnote_text.replace(".htm", '.json')
+
             #Creates the current output file
             main_file = main_text
             out_main = open(main_file, "w", encoding="utf-8")
+
+            footnote_file = footnote_text
+            out_footnote = open(footnote_file, "w", encoding="utf-8")
 
             curr_section = ""
 
@@ -68,61 +90,76 @@ def main():
                         if len(chap_cont) >= 0 and chap_cont[0] != "\n":
                             #format_main(curr_soup.find("p", class_="footnote").contents, out_footnote)
                             if 3 <= i <= 38 and curr_soup.find("p", class_="footnote"):
-                                footnote_text = x
-                                footnote_text = footnote_text.replace("OEBPS", 'Footnotes')
-                                footnote_text = footnote_text.replace(".htm", '.json')
-
-                                footnote_file = footnote_text
-                                out_footnote = open(footnote_file, "w", encoding="utf-8")
-
                                 format_footnote(curr_soup.find("p", class_="footnote").contents, out_footnote)
-
-                                out_footnote.close()
                             else:
                                 if curr_soup.find(class_="event"):
-                                    if check_is_next_dict(curr_soup.find(class_="event").contents, curr_dict):
+                                    chap_cont = curr_soup.find(class_="event").contents
+
+                                    if check_is_next_dict(curr_soup.find(class_="event").contents, main_dict):
                                         if last_soup and last_soup.find(class_="lines_float"):
-                                            temp_reference = curr_dict["cross-references"].pop()
-                                            curr_dict = create_main_dict(print_main_dict(curr_dict, out_main))
-                                            curr_dict["cross-references"].append(temp_reference)
-                                            curr_dict["before"] = curr_section
+                                            temp_reference = main_dict["cross-references"].pop()
+                                            create_main_dict(print_main_dict(out_main))
+                                            main_dict["cross-references"].append(temp_reference)
+                                            main_dict["before"] = curr_section
 
                                         else:
-                                            curr_dict = create_main_dict(print_main_dict(curr_dict, out_main))
-                                            curr_dict["before"] = curr_section
+                                            create_main_dict(print_main_dict(out_main))
+                                            main_dict["before"] = curr_section
                                     else:
-                                        curr_dict["before"] = curr_section
+                                        main_dict["before"] = curr_section
+
+                                    if i == -1: print(chap_cont)
+
+                                    add_text(chap_cont)
 
                                 elif curr_soup.find(class_="event01"):
-                                    if check_is_next_dict(curr_soup.find(class_="event01").contents, curr_dict):
-                                        curr_dict = create_main_dict(print_main_dict(curr_dict, out_main))
-                                        curr_dict["before"] = curr_section
+                                    chap_cont = curr_soup.find(class_="event01").contents
+
+                                    if check_is_next_dict(chap_cont, main_dict):
+                                        create_main_dict(print_main_dict(out_main))
+                                        main_dict["before"] = curr_section
                                     else:
-                                        curr_dict["before"] = curr_section
+                                        main_dict["before"] = curr_section
+
+                                    if i == -1: print(chap_cont)
+                                    add_text(chap_cont)
+
+                                elif curr_soup.find(class_="event1"):
+                                    chap_cont = curr_soup.find(class_="event1").contents
+
+                                    if i == -1: print(chap_cont)
+                                    add_text(chap_cont)
+
+                                elif curr_soup.find(class_="event_indent"):
+                                    chap_cont = curr_soup.find(class_="event_indent").contents
+
+                                    if i == -1: print(chap_cont)
+                                    add_text(chap_cont)
 
                                 elif curr_soup.find(class_="lines_float"):
-                                    curr_dict["cross-references"].append(curr_soup.find(class_="lines_float").contents[0].string)
+                                    main_dict["cross-references"].append(curr_soup.find(class_="lines_float").contents[0].string)
 
                                 elif curr_soup.h1:
-                                    print(curr_soup.h1['id'])
                                     curr_section = curr_soup.h1['id']
 
                                 elif curr_soup.h2:
-                                    print(curr_soup.h2['id'])
                                     curr_section = curr_soup.h2['id']
 
                                 elif curr_soup.find(class_="eventsection"):
                                     if "id" in curr_soup.find(class_="eventsection").attrs:
-                                        print(curr_soup.find(class_="eventsection")['id'])
                                         curr_section = curr_soup.find(class_="eventsection")['id']
 
                                 #else:
-                                    #print("Prevet")
+                                    #print("Prevet" + curr_soup)
                             last_soup = curr_soup
 
-            curr_dict = create_main_dict(print_main_dict(curr_dict, out_main))
+            create_main_dict(print_main_dict(out_main))
             out_main.close()
+            out_footnote.close()
         i = i + 1
+
+        global page_number
+        page_number = i
 
 
 def check_is_next_dict(soup, curr):
@@ -137,39 +174,49 @@ def check_is_next_dict(soup, curr):
         return False
 
 
-def check_if_dict_empty(curr) :
+def check_if_dict_empty():
 
-    if curr["text"] == "":
+    if main_dict["text"] == "":
         return True
     else:
         return False
 
-#def check_is_wrong_references():
 
-
-
-def print_main_dict(curr, out):
+def print_main_dict(out):
 
     out.write("{\n")
-    for x, y in curr.items():
-        out.write("\t" + str(x) + ": " + str(y) + "\n")
+    for x, y in main_dict.items():
+        if x == "cross-references":
+            out.write("\t" + str(x) + ": ")
+            for z in main_dict["cross-references"]:
+                if z != main_dict["cross-references"][len(main_dict["cross-references"])-1]:
+                    out.write(str(z) + ", ")
+                else:
+                    out.write(str(z))
+            out.write("\n")
+        elif x == "footnotes":
+            out.write("\t" + str(x) + ": ")
+            for z in main_dict["footnotes"]:
+                if z != main_dict["footnotes"][len(main_dict["footnotes"])-1]:
+                    out.write(str(z) + ", ")
+                else:
+                    out.write(str(z))
+            out.write("\n")
+        else:
+            out.write("\t" + str(x) + ": " + str(y) + "\n")
     out.write("}\n")
 
-    return curr["number"]+1
+    return main_dict["number"]+1
 
 
 def create_main_dict(num):
 
-    main_dict = {
-        "number": 0,
-        "text": "",
-        "before": "",
-        "after": "",
-        "cross-references": [],
-        "footnotes": []
-    }
-
     main_dict["number"] = num
+    main_dict["text"] = ""
+    main_dict["before"] = ""
+    main_dict["after"] = ""
+    main_dict["cross-references"] = []
+    main_dict["footnotes"] = []
 
     return main_dict
 
@@ -195,11 +242,43 @@ def format_footnote(string, out):
 
     out.write("{\n")
     for x, y in foot_dict.items():
+        if page_number == -1: print("\t"+str(x)+": "+str(y)+"\n")
         out.write("\t"+str(x)+": "+str(y)+"\n")
     out.write("}\n")
 
 
-#def format_main(string, out):
+def add_text(chap_list):
+
+    for item in chap_list:
+        check = not check_if_invalid_text(item, chap_list)
+
+        text = stringify(item, 0)
+        if page_number == -1:
+            print(str(check) + "  " + str(item))
+
+        if check:
+            if text.isdigit():
+                main_dict["footnotes"].append(text)
+                main_dict["text"] = main_dict["text"] + "@"
+            else:
+                main_dict["text"] = main_dict["text"] + text
+
+    main_dict["text"] = main_dict["text"] + " "
+
+    return main_dict
+
+
+def check_if_invalid_text(string, list_of_str):
+
+    if string == "\n":
+        if page_number == -1: print(1)
+        return True
+    elif string.string:
+        if string.string.isdigit() and string == list_of_str[0]:
+            if page_number == -1: print(2)
+            return True
+    else:
+        return False
 
 
 def stringify(x, i):
@@ -208,6 +287,8 @@ def stringify(x, i):
     if len(x) != 0:
         for y in x:
             curr = curr + str(y)
+
+    if page_number == -1: print(x)
 
     if x.string == x:
         return str(x)
@@ -229,23 +310,29 @@ def stringify(x, i):
         for y in x.contents:
             temp = temp + stringify(y, i)
         return temp + "**"
-    #elif str(x) == '<sup class="frac">' + curr + '</sup>':
-    #    temp = ""
+    elif str(x) == '<sup class="frac">' + curr + '</sup>':
+        temp = ""
 
-    #   for y in x.contents:
-    #        temp = temp + stringify(y, i)
-    #   return "[^" + temp + "]"
-    elif str(x) == "<br/>":
-        if i == 0:
-            return "\n"
-        else:
-            j = 0
-            temp = ""
+        for y in x.contents:
+            main_dict["footnotes"].append(y)
+            temp = temp + "@"
+        return temp
+    elif "class" in x.attrs and "hlink" in x.attrs["class"]:
+        if "id" in x.attrs:
+            temp = x.attrs["id"]
 
-            while j != i:
-                temp = temp + "#"
-                j += 1
-            return "\n" + temp
+            if x.contents and x.contents[0].name == "sup":
+                for y in x.contents:
+                    temp = "["+stringify(y, i) + "](" + x.contents[0].contents[0] + ")"
+                return temp
+            else: return "{"+temp+"}"
+        elif "href" in x.attrs:
+            temp = x.attrs["href"][x.attrs["href"].find('#'):]
+            temp = temp[1:]
+
+            for y in x.contents:
+                temp = "[" + stringify(y, i) + "](" + temp + ")"
+            return temp
     elif len(x.contents) != 1:
         temp = ""
 
