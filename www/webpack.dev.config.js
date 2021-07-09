b233502d6a96bc86e5b1b8de09c1f91c2261582c
Peter DeVita <mewtwo2643@yahoo.com>
2021-07-09 18:34:49 -0400

    Fix dev server not serving changes

--- www/webpack.dev.config.js
+++ www/webpack.dev.config.js
@@ -9,7 +9,7 @@ module.exports = (env, options) => {
     let thing = config(env, options);
 
     thing.module.rules[0]['include'] = [path.resolve(__dirname, 'src')]  // Prefresh freaks the heck out without this
-    thing.plugins.splice(1, 1);  // Remove MiniCSS plugin
+    thing.plugins.splice(2, 1);  // Remove MiniCSS plugin
     thing.plugins.splice(0, 1); // Remove Clean Webpack plugin
     thing.plugins.push(new webpack.HotModuleReplacementPlugin());
     thing.plugins.push(new PreactRefreshPlugin());
